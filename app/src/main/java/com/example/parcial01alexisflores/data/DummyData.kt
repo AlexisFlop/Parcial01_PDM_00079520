e50908a4bb2f42496f8382d289a6b9156fcba267
AlexisFlop <00079520@uca.edu.sv>
2023-05-22 14:07:06 -0600

    New model

---
+++ app/src/main/java/com/example/parcial01alexisflores/data/DummyData.kt
@@ -0,0 +1,8 @@
+package com.example.parcial01alexisflores.data
+
+import com.example.parcial01alexisflores.data.model.FruitModel
+
+var fruitList = mutableListOf(
+    FruitModel("Sandia", "Rojo"),
+    FruitModel("Pera", "Verde")
+)
---
+++ app/src/main/java/com/example/parcial01alexisflores/data/model/FruitModel.kt
@@ -0,0 +1,6 @@
+package com.example.parcial01alexisflores.data.model
+
+data class FruitModel(
+    var name: String,
+    var color: String
+)
